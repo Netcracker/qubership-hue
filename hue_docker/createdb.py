557b0ddbd925545f8e8ff60c51ec6223c8e29911
Mohanakrishna Sadhu <mohanakrishna.sadhu@netcracker.com>
2025-04-24 17:45:50 +0530

    fixed with pyhton lint

--- hue_docker/createdb.py
+++ hue_docker/createdb.py
@@ -1,4 +1,7 @@
+"""Module for creating and managing a PostgreSQL database."""
+
 import os
+
 import psycopg2
 
 postgres_host = os.getenv("POSTGRES_HOST")
@@ -10,7 +13,9 @@ postgres_admin_user = os.getenv("POSTGRES_ADMIN_USER")
 postgres_admin_password = os.getenv("POSTGRES_ADMIN_PASSWORD")
 sslmode = os.getenv("POSTGRES_SSLMODE")
 
-check_if_db_exists = f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{db_name}';"
+check_if_db_exists = (
+    f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{db_name}';"
+)
 create_db = f"CREATE DATABASE {db_name};"
 check_if_user_exists = f"SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = '{db_user}';"
 create_user = f"CREATE USER {db_user} PASSWORD '{db_password}';"
@@ -18,8 +23,12 @@ grant_permissions = f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {db_user};"
 grant_ownership = f"ALTER DATABASE {db_name} OWNER TO {db_user};"
 
 conn = psycopg2.connect(
-    database="postgres", user=postgres_admin_user, password=postgres_admin_password,
-    host=postgres_host, port=postgres_port, sslmode=sslmode
+    database="postgres",
+    user=postgres_admin_user,
+    password=postgres_admin_password,
+    host=postgres_host,
+    port=postgres_port,
+    sslmode=sslmode,
 )
 
 conn.autocommit = True
